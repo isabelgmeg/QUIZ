3ab05997535a3fe99930b9ebe4d67ed1a7ad2f08
Isabelgarcia <isabelgmegino@gmail.com>
2020-10-26 19:27:44 +0100

    update con sol

---
+++ quiz.js
@@ -0,0 +1,44 @@
+const myQuestions = [
+    {
+      question: "¿Qué actor de Hollywood vivió en la calle durante unos meses hasta que su carrera despegó?",
+      answers: {
+        a: "Niro",
+        b: "Stallone",
+        c: "Harrison",
+        d: "Willis"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "¿Sabes cual es el origen del icónico código verde que puede verse a lo largo de todos los films de la saga de películas “Matrix”?",
+      answers: {
+        a: "Codigo",
+        b: "Sushi",
+        c: "Manga",
+        d: "Aleatorio"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "¿En qué película se usa más veces la palabra “Fuck”?",
+      answers: {
+        a: "Pulp",
+        b: "Wolf",
+        c: "Fuckity",
+        d: "Swearnet"
+      },
+      correctAnswer: "a"
+    },
+    {
+        question: "¿Cuáles de los siguientes personajes sirvieron de inspiración para crear personajes de la saga Star Wars?",
+        answers: {
+            a: "Clint",
+            b: "Einstein",
+            c: "Laura-Hardy",
+            d: "Astaire"
+        }
+        correctAnswer: {
+            "a","b", "c"
+        }
+    }
+  ];
--- scripts.js
+++ scripts.js
@@ -1,36 +1,50 @@
 function handleSubmit(event) {
 event.preventDefault(); // Ya no se recarga la página!! 🎉
 }
-//elementos del formulario
-//?? const formElements = document.querySelector('#formulario-cine')
-const formElements = onsubmit.target.elements;
 
-//elementos por separado
-const firstqq = formElements['first-qq'].value;
-const secondqq = formElements['second-qq'].value;
-const thirdqq = formElements['third-qq'].value;
-const fourthqq = formElements['fourth-qq'].value;
-const fifthqq = formElements['fifth-qq'].value;
+//elementos del formulario
+const formElements = document.querySelector('#formulario-cine')
+//formElements = onclick.formElements.elements;
+console.log(formElements)
 
 //Primera función
 //seleccionamos el boton y lo metemos como constante
-const submitFq = document.querySelector("#submit-fq")
+const buttonFq = document.querySelector("#button-fq")
+
+//elementos por separado
+const firstqq = document.querySelector('#first-qq')
+// const secondqq = formElements['second-qq'].value;
+// const thirdqq = formElements['third-qq'].value;
+// const fourthqq = formElements['fourth-qq'].value;
+// const fifthqq = formElements['fifth-qq'].value;
 
 //Añadimos event listener al boton submit
-SubmitFq.addEventListener('onsubmit',handleFirstQ)
+buttonFq.addEventListener('click',handleFirst)
+
 
 //Declaramos resuesta incorrecta a preguntas
 const RespuestaIncorrecta = 'Tu respuesta es incorrecta'
 
+const curiosityFirstQcontent = document.querySelector("#curiosity-first-q");
+
+const answerFirstQ = document.querySelector("#button-fq");
+
 //Declaramos la funcion
-function handleFirstQ (){
+function handleFirst(click) {
+    console.log(firstqq)
     if(firstqq === "Stallone"){
-        console.log('Vas bien')
-        return curiosityFirstQcontent = 'Tu respuesta es correcta ¿Sabías que Sylvester Stallone a principios de los 70 Stallone se traslada a Nueva York, persiguiendo el sueño de triunfar en Hollywood, pero la falta de papeles acaba obligándolo a vivir en una estación de autobús durante 3 meses? Durante estos tres meses su necesidad fue tal que tuvo que vender a su perro por 25$, al que recuperaría meses después por 3000$ tras escribir el guión de Rocky'
+        return answerFirstQ.innerText= 'Tu respuesta es correcta ¿Sabías que Sylvester Stallone a principios de los 70 Stallone se traslada a Nueva York, persiguiendo el sueño de triunfar en Hollywood, pero la falta de papeles acaba obligándolo a vivir en una estación de autobús durante 3 meses? Durante estos tres meses su necesidad fue tal que tuvo que vender a su perro por 25$, al que recuperaría meses después por 3000$ tras escribir el guión de Rocky'
+        
+        console.log('correcto')
+        alert('Vs bien')
+
+        function handleSolutionFirst(){
+            return curiosityFirstQcontent.innerText = 'Tu respuesta es correcta ¿Sabías que Sylvester Stallone a principios de los 70 Stallone se traslada a Nueva York, persiguiendo el sueño de triunfar en Hollywood, pero la falta de papeles acaba obligándolo a vivir en una estación de autobús durante 3 meses? Durante estos tres meses su necesidad fue tal que tuvo que vender a su perro por 25$, al que recuperaría meses después por 3000$ tras escribir el guión de Rocky'
+        }
     }
     else{
-        return RespuestaIncorrecta
+        return 'hOLA'
     }
 }
-const curiosityFirstQcontent = curiosityFirstQ.innerText
-const curiosityFirstQ = documentQuerySelector("#curiosity-first-q");
+
+
